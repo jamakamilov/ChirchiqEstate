802c643861352010ddbd43b7baefb992a3ca8e3e
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:21:26 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+from sqlalchemy.dialects.postgresql import JSONB
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, index=True, nullable=False)
+    phone = Column(String, nullable=True)
+    name = Column(String, nullable=True)
+    language = Column(String, default="uz")
+    city = Column(String, nullable=True)
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    role = relationship("Role", back_populates="users")
+    favorites = relationship("Favorite", back_populates="user")
