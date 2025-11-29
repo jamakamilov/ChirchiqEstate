337b2dbc7bb3618debd38ccb80fe0af51bb28e91
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:26:10 +0500

    Update admin_menu.py

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -1,1 +1,9 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def admin_main():
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton("Модерация объявлений", callback_data="admin_mod_listings"))
+    kb.add(InlineKeyboardButton("Модерация оплат", callback_data="admin_mod_payments"))
+    kb.add(InlineKeyboardButton("Комиссии", callback_data="admin_commissions"))
+    kb.add(InlineKeyboardButton("Статистика", callback_data="admin_stats"))
+    return kb
