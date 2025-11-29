5edc49ce05ebc07071c9d0f538a61f2903987bca
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:23:25 +0500

    Update favorites.py

--- models/favorites.py
+++ models/favorites.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+from sqlalchemy.sql import func
 
+class Favorite(Base):
+    __tablename__ = "favorites"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    listing_id = Column(Integer, ForeignKey("listings.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="favorites")
