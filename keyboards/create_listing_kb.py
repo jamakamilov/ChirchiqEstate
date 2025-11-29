a1ac67e8fdeba3b5adef5d9136c59d481fe79767
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:26:28 +0500

    Update create_listing_kb.py

--- keyboards/create_listing_kb.py
+++ keyboards/create_listing_kb.py
@@ -1,1 +1,14 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def category_kb():
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton("Квартира", callback_data="cat_apartment"))
+    kb.add(InlineKeyboardButton("Дом", callback_data="cat_house"))
+    kb.add(InlineKeyboardButton("Земля", callback_data="cat_land"))
+    return kb
+
+def deal_type_kb():
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton("Продажа", callback_data="deal_sale"))
+    kb.add(InlineKeyboardButton("Аренда", callback_data="deal_rent"))
+    return kb
