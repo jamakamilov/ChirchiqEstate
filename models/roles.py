b3660bac2e21b39dfbf6e9e8d216274d145334c8
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:21:44 +0500

    Update roles.py

--- models/roles.py
+++ models/roles.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from database import Base
 
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=True)
+    is_paid = Column(Boolean, default=False)
+    active = Column(Boolean, default=True)
+
+    users = relationship("User", back_populates="role")
