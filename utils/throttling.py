3d498fd8ac8629aae4899563cc7a241ff449b883
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:25:28 +0500

    Update throttling.py

--- utils/throttling.py
+++ utils/throttling.py
@@ -1,1 +1,18 @@
+import asyncio
+from collections import defaultdict
+from time import time
 
+class Throttler:
+    def __init__(self, rate=1, per=1.0):
+        self.rate = rate
+        self.per = per
+        self.calls = defaultdict(list)
+
+    async def throttle(self, key):
+        now = time()
+        q = self.calls[key]
+        q = [t for t in q if t > now - self.per]
+        if len(q) >= self.rate:
+            await asyncio.sleep(self.per - (now - q[0]))
+        q.append(time())
+        self.calls[key] = q
