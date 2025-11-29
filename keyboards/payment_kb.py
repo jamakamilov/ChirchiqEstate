060f99de6b8e6b51133a58f3294df0e068621fe4
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:26:48 +0500

    Update payment_kb.py

--- keyboards/payment_kb.py
+++ keyboards/payment_kb.py
@@ -1,1 +1,7 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def payment_actions():
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton("Отправить скрин", callback_data="send_screenshot"))
+    kb.add(InlineKeyboardButton("Отмена", callback_data="cancel_payment"))
+    return kb
