6e15a992fe87a4d52a3a32f8cbe0ff13ebc7756d
Jamshid <43925306+jamakamilov@users.noreply.github.com>
2025-11-29 12:24:09 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,9 @@
+import logging
+import sys
 
+logger = logging.getLogger("realestate_bot")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
